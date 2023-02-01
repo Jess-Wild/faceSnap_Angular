1cf6bb3607acb5cd461460e2aaa6fe6243891fd0
Jess-Wild <j.victorion.wcs@gmail.com>
2023-02-01 16:56:26 +0100

    Add Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  interval$!: Observable<number>;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.interval$ = interval(1000);
+
+  }
 }
