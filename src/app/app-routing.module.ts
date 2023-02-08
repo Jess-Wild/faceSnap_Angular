1b3cac8537ff4ba719691975a34a5f5c42be6a69
Jess-Wild <j.victorion.wcs@gmail.com>
2023-02-08 14:52:06 +0100

    Add Button onAddNewFaceSnap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
 import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
@@ -5,9 +6,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 
 const routes: Routes = [
-  { path: "facesnaps/:id", component: SingleFaceSnapComponent},
- {  path: "facesnaps", component: FaceSnapListComponent },
- {path:"", component: LandingPageComponent}
+  { path: "facesnaps/:id", component: SingleFaceSnapComponent },
+  { path: "facesnaps", component: FaceSnapListComponent },
+  { path: "create", component: NewFaceSnapComponent },
+  { path:"", component: LandingPageComponent  }
 ];
 
 @NgModule({
